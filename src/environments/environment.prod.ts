daadabcf698de43dac84c9656a95a96899d28e82
Stierle <paul.stierle@pf2542k6dk.fritz.box>
2022-10-17 11:30:45 +0200

    url fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://oyster-app-jqjom.ondigitalocean.app',
-// };
+ export const environment = {
+   production: true,
+   apiUrl: 'https://oyster-app-jqjom.ondigitalocean.app',
+};
 
-export const environment = {
+/* export const environment = {
   production: false,
   apiUrl: "http://localhost:8080",
-};
+}; */
