f37244602495f4f4d9b4a8455ce81cc732b5a6e5
pstierle <p.stierle@web.de>
2022-10-20 11:37:02 +0200

    style changes

--- src/app/components/icons/icon.module.ts
+++ src/app/components/icons/icon.module.ts
@@ -1,10 +1,21 @@
-import { UpArrowIconComponent } from './up-arrow-icon';
-import { QuestionIconMarkComponent } from './question-mark-icon.compontent';
-import { NgModule } from '@angular/core';
-import { CheckIconComponent } from './check-icon.compontent';
+import { GoldIconComponent } from "./gold-icon.component";
+import { UpArrowIconComponent } from "./up-arrow-icon";
+import { QuestionIconMarkComponent } from "./question-mark-icon.compontent";
+import { NgModule } from "@angular/core";
+import { CheckIconComponent } from "./check-icon.compontent";
 
 @NgModule({
-  declarations: [QuestionIconMarkComponent, CheckIconComponent, UpArrowIconComponent],
-  exports: [QuestionIconMarkComponent, CheckIconComponent, UpArrowIconComponent],
+  declarations: [
+    QuestionIconMarkComponent,
+    CheckIconComponent,
+    UpArrowIconComponent,
+    GoldIconComponent,
+  ],
+  exports: [
+    QuestionIconMarkComponent,
+    CheckIconComponent,
+    UpArrowIconComponent,
+    GoldIconComponent,
+  ],
 })
 export class IconModule {}
