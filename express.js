ef88e4e3445ebbf6405fda76a946f3f77f59d41d
pstierle <p.stierle@web.de>
2022-12-26 12:16:39 +0100

    path fix

--- express.js
+++ express.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 3000;
 const path = require("path");
 const distPath = path.join(__dirname, "dist");
-app.use(distPath);
+
+app.use(express.static(distPath));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
